23d38cd62e1b9b85883852197b7299b15510bf0f
blazhkevych.v <blazhkevych.v@gmail.com>
2023-07-25 16:03:56 +0300

    #Task № 3. Реализация главного меню приложения. Добавлен класс "Main_Class". В данном классе реализовано главное меню программы, которое предоставляет пользователю список доступных операций для работы с классом "Academy_Group". Каждая операция соответствует определенному выбору пользователя.
    Главное меню является важной частью пользовательского интерфейса приложения, позволяющей пользователю выбирать нужные действия. Создание и реализация главного меню позволяет предоставить удобный и понятный интерфейс для взаимодействия с программой. В данном случае, главное меню предоставляет выбор добавления, удаления, редактирования, печати и поиска студентов в академической группе. Также предусмотрен пункт для выхода из программы. Все операции взаимодействия с классом "Academy_Group" реализованы через вызов соответствующих методов, обеспечивая эффективную работу с данными.

---
+++ List of storage media/Main_Class.cs
@@ -0,0 +1,103 @@
+﻿using Academy_GroupNamespace;
+
+namespace task
+{
+    // Реализует пользовательский интерфейс приложения, и 
+    // демонстрируюет работу с классом Academy_Group.
+    internal class Main_Class
+    {
+        // Главное меню.
+        public void MainMenu(ref Academy_Group ag)
+        {
+            string choice = "";
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@"1. Добавление студентов в группу.
+2. Удаление студента из группы.
+3. Редактирование данных студента.
+4. Печать академической группы.
+5. Поиск студента по заданному критерию.
+6. Выход из программы.");
+                Console.WriteLine();
+                Console.WriteLine("Я выбираю: ");
+
+                try
+                {
+                    choice = Convert.ToString(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Введите количество студентов для добавления:");
+                        int amountOfStudentsToAdd = -1;
+                        do
+                        {
+                            try
+                            {
+                                amountOfStudentsToAdd = Convert.ToInt32(Console.ReadLine());
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                                Console.WriteLine();
+                                Console.WriteLine("Попробуйте еще раз !");
+                            }
+                        } while (amountOfStudentsToAdd < 0);
+                        ag.AddStudent(amountOfStudentsToAdd);
+                        Console.WriteLine();
+                        Console.WriteLine("Для продолжения нажмите любую клавишу.");
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        ag.PrintGroup();
+                        Console.WriteLine();
+                        Console.WriteLine("Введите № студента для удаления:");
+                        int studentNumberToRemove = -1;
+                        try
+                        {
+                            studentNumberToRemove = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        ag.Remove(studentNumberToRemove);
+                        Console.WriteLine();
+                        Console.WriteLine("Для продолжения нажмите любую клавишу.");
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        ag.Edit();
+                        Console.WriteLine();
+                        Console.WriteLine("Для продолжения нажмите любую клавишу.");
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        ag.PrintGroup();
+                        Console.WriteLine();
+                        Console.WriteLine("Для продолжения нажмите любую клавишу.");
+                        Console.ReadLine();
+                        break;
+                    case "5":
+                        ag.Search();
+                        Console.WriteLine();
+                        Console.WriteLine("Для продолжения нажмите любую клавишу.");
+                        Console.ReadLine();
+                        break;
+                    case "6":
+                        ag.Save();
+                        Environment.Exit(0);
+                        break;
+                }
+                MainMenu(ref ag);
+            } while (choice != "1" | choice != "2" | choice != "3" | choice != "4" | choice != "5" | choice != "6" | choice != "7");
+        }
+    }
+}
