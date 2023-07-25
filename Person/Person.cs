fa714de6f41fc65cf27c97c31d284dc68b55b6f8
blazhkevych.v <blazhkevych.v@gmail.com>
2023-07-25 16:20:17 +0300

    #Task № 5. Добавление свойств и метода "Print" в класс "Person".
    В класс "Person" добавлены свойства "Name", "Surname", "Age" и "Phone", которые позволяют получать и устанавливать значения соответствующих полей класса. Также добавлен метод "Print", который выводит информацию о человеке (имя, фамилия, возраст и телефон) на экран.
    
    Добавление свойств "Name", "Surname", "Age" и "Phone" обеспечивает контролируемый доступ к полям класса "Person". Теперь значения этих полей могут быть получены извне класса через соответствующие свойства или установлены с помощью свойств с проверкой на допустимость значений. Это позволяет избежать прямого доступа к полям и обеспечивает более гибкую и безопасную работу с данными класса.
    Метод "Print" добавлен для удобного вывода информации о человеке на экран. Это может быть полезно при отладке или тестировании программы, а также для представления данных пользователю в удобном формате. Метод "Print" выводит значения имени, фамилии, возраста и телефона, используя форматирование для выравнивания текста при выводе.

--- Person/Person.cs
+++ Person/Person.cs
@@ -18,5 +18,39 @@
             _Age = age;
             _Phone = phone;
         }
+
+        // Свойство Name.
+        public string Name
+        {
+            get => _Name;
+            set => _Name = value;
+        }
+
+        // Свойство Surname.
+        public string Surname
+        {
+            get => _Surname;
+            set => _Surname = value;
+        }
+
+        // Свойство Age.
+        public int Age
+        {
+            get => _Age;
+            set => _Age = value;
+        }
+
+        // Свойство Surname.
+        public string Phone
+        {
+            get => _Phone;
+            set => _Phone = value;
+        }
+
+        // Вывод информации на экран.
+        public void Print()
+        {
+            Console.Write("{0,20}{1,20}{2,9}{3,17}", _Name, _Surname, _Age, _Phone);
+        }
     }
 }
