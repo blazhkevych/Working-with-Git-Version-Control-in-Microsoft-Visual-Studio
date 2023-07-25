454215c28406116d78f09de7db7c814858c1d583
blazhkevych.v <blazhkevych.v@gmail.com>
2023-07-25 16:15:15 +0300

    #Task № 4. Класс "Person" с конструкторами и полями.
    В данном пространстве имен "PersonNamespace" определен класс "Person", содержащий защищенные поля для хранения имени, фамилии, возраста и телефона. Класс обладает двумя конструкторами: конструктором по умолчанию, который устанавливает значения полей по умолчанию, и конструктором с параметрами, который позволяет установить значения полей при создании объекта класса.
    
    Класс "Person" представляет общую сущность человека с базовыми атрибутами, такими как имя, фамилия, возраст и телефон. Конструкторы позволяют создавать объекты класса с заданными атрибутами или с значениями по умолчанию, что обеспечивает гибкость при использовании класса в других частях программы. Защищенные поля позволяют обеспечить контроль доступа к данным класса и защитить их от прямого изменения извне. В дальнейшем этот класс может использоваться как базовый класс для производных классов с более специализированными атрибутами или функциональностью.

---
+++ Person/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonNamespace
+{
+    public class Person
+    {
+        protected string _Name;     // Имя.
+        protected string _Surname;  // Фамилия.
+        protected int _Age;          // Возраст.
+        protected string _Phone;    // Телефон.
+
+        // Конструктор по умолчанию.
+        public Person() : this("неизвестно", "неизвестно", 0, "неизвестно") { }
+
+        // Конструктор с параметрами.
+        public Person(string name, string surname, int age, string phone)
+        {
+            _Name = name;
+            _Surname = surname;
+            _Age = age;
+            _Phone = phone;
+        }
+    }
+}
