0d7ccb70d82e78ab2b056a62cba0dea1e3114d76
blazhkevych.v <blazhkevych.v@gmail.com>
2023-07-25 16:32:36 +0300

    #Task № 6. Класс "Student" наследующий класс "Person".
    В пространстве имен "StudentNamespace" определен класс "Student", который наследует функциональность от класса "Person". Класс "Student" содержит дополнительные поля: "_Average" для хранения среднего балла студента и "_NumberOfGroup" для хранения номера группы.
    
    Класс "Student" расширяет класс "Person" и добавляет дополнительные атрибуты, которые уникальны для студента, такие как средний балл и номер группы. Использование наследования позволяет избежать дублирования кода и упрощает работу с общими атрибутами, такими как имя, фамилия, возраст и телефон, которые уже определены в базовом классе "Person". Таким образом, при создании объекта класса "Student", можно использовать конструктор базового класса "Person" для инициализации общих атрибутов, а затем задать специфичные для студента атрибуты с помощью дополнительных конструкторов класса "Student". Это обеспечивает удобство и чистоту кода при работе с данными о студентах.

---
+++ Student/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentNamespace
+{
+    using PersonNamespace;
+    using System;
+    using System.Xml.Linq;
+
+    public class Student : Person
+    {
+        protected double _Average;            // Средний балл.
+        protected string _NumberOfGroup;      // Номер группы.
+
+        // Конструктор по умолчанию.
+        public Student() : this("неизвестно", "неизвестно", 0, "неизвестно", 0.0, "неизвестно") { }
+
+        // Конструктор с параметрами.
+        public Student(string name, string surname, int age, string phone, double average, string numberOfGroup)
+            : base(name, surname, age, phone)
+        {
+            _Average = average;
+            _NumberOfGroup = numberOfGroup;
+        }
+    }
+}
